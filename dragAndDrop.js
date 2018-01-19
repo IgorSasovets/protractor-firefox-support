36c6eea09ba0fab870b733d58ce68a7bc9e8aa2a
Ihor <ihor.sasovets@gmail.com>
2018-01-19 19:39:57 +0200

    Added possibility to set custom options for method

--- dragAndDrop.js
+++ dragAndDrop.js
@@ -1,39 +1,56 @@
 'use strict';
 /**
- * Originally from https://ghostinspector.com/blog/simulate-drag-and-drop-javascript-casperjs/
+ * Originally from: https://ghostinspector.com/blog/simulate-drag-and-drop-javascript-casperjs/,
+ * https://github.com/kemokid/scripting-sortable/blob/master/script_sortable_dnd_more_general.js.
  * Modified by IgorSasovets (https://github.com/IgorSasovets) to simulate drag and drop movement
- * in Firefox browser (v.57.0.4)
+ * in Firefox browser (v.57.0.4).
  */
 
-module.exports.triggerDragAndDrop = `function dragAndDrop(selectorDrag, selectorDrop, callback) {
-    // fetch target elements
-
-    var elements = document.querySelectorAll("<element_selector>");
-    $(elements[1]).draggable(); //makes elements draggable
-    $(elements[1]).click();
-
-    var elemDrag = elements[1];
-    var elemDrop = document.querySelector("<element_selector>");
-
-    if (!elemDrag || !elemDrop) {
-        console.log("can't get elements");
-        return false;
+module.exports.dragAndDrop = `function dragAndDrop(selectorDrag, selectorDrop, options) {
+    
+    let elemDrag, elemDrop;
+    
+    /*******************************GET ELEMENTS********************************/ 
+    
+    if ((!selectorDrag && selectorDrag != null) || (!selectorDrop && selectorDrop != null)) {
+        throw new Error('Error!Element selector not defined.');
+    }
+    
+    if (options.dragElemIndex) {
+        const elements = document.querySelectorAll(selectorDrag);
+        elemDrag = elements[options.dragElemIndex];
+    } else {
+        elemDrag = document.querySelector(selectorDrag);
+    }
+    
+    elemDrop = (selectorDrop === null && options.dropLocation) ? null : document.querySelector(selectorDrop);
+    
+    /***************************************************************************/
+    
+    /*************************CHECK USER CUSTOM OPTIONS*************************/
+    
+    if (options.makeDraggable)
+         makeDraggable(elements[1]);
+    
+    /***************************************************************************/  
+    
+    //function for make element draggable
+    function makeDraggable(elm) {
+        $(elm).draggable();
     }
 
     // function for triggering mouse events
     function fireMouseEvent(type, elem, dataTransfer) {
-        var evt = document.createEvent('MouseEvents');
+        const evt = document.createEvent('MouseEvents');
         evt.initMouseEvent(type, true, true, window, 1, 1, 1, 0, 0, false, false, false, false, 0, elem);
         if (/^dr/i.test(type)) {
             evt.dataTransfer = dataTransfer || createNewDataTransfer();
         }
-
-        elem.dispatchEvent(evt);
-        return evt;
+        (elem === null) ? document.body.dispatchEvent(evt) : elem.dispatchEvent(evt);
     }
 
     function createNewDataTransfer() {
-        var data = {};
+        let data = {};
         return {
             clearData: function(key) {
                 if (key === undefined) {
@@ -58,17 +75,22 @@ module.exports.triggerDragAndDrop = `function dragAndDrop(selectorDrag, selector
     }
     
     function mouseMove() {
-        var evt = document.createEvent('MouseEvents');
-        evt.initMouseEvent('mousemove', true, true, window, 1, 10, 20, 200, 200, false, false, false, false, 0, null);
+        const evt = document.createEvent('MouseEvents');
+        if (elemDrop === null) {
+            evt.initMouseEvent('mousemove', true, true, window, 1, 1, 1, options.dropLocation.x,
+             options.dropLocation.y, false, false, false, false, 0, null);
+        } else {
+             evt.initMouseEvent('mousemove', true, true, window, 1, 1, 1, 0, 0, false, false, false,
+                false, 0, elemDrop);
+        }
         if (/^dr/i.test('mousemove')) {
             evt.dataTransfer = dataTransfer || createNewDataTransfer();
         }
-        document.body.dispatchEvent(evt);
+        (elemDrop === null) ? document.body.dispatchEvent(evt) : elemDrop.dispatchEvent(evt);
     }
     
     function drop() {
-        console.log('DROP');
-        fireMouseEvent('mouseup', elemDrop); // not strictly necessary but I like the symmetry
+        fireMouseEvent('mouseup', null);
     }
     
     fireMouseEvent('mousedown', elemDrag);
