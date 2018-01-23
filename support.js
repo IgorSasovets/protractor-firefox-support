25fb39663258d64b4e10085bb3d8af942f64bdb1
Ihor <ihor.sasovets@gmail.com>
2018-01-23 20:01:20 +0200

    - changed structure of repository, added mouse up function

---
+++ support.js
@@ -0,0 +1,9 @@
+const dragAndDrop = require('./lib/dragAndDrop'),
+	mouseUp = require('./lib/mouseUp'),
+	rightMouseBtnClick = require('./lib/rightMouseButtonClick');
+
+module.exports = {
+	dragAndDrop: dragAndDrop,
+	mouseUp: mouseUp,
+	rightMouseBtnClick: rightMouseBtnClick
+}
