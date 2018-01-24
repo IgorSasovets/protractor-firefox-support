0cbbb559322ecde4981338f4f1aaac457ae4dddf
Ihor <ihor.sasovets@gmail.com>
2018-01-24 12:20:55 +0200

    - added examples of usage

---
+++ examples/examplesOfUsage.js
@@ -0,0 +1,16 @@
+const helper = require('../support');
+
+async performDnD({dragElementSelector = '<selector>', dragElementIndex = <some_index>,
+    draggable = false} = {}) {
+    const dropPoint = {x: 100, y: 100};
+    const options = {makeDraggable: draggable, dragElemIndex: dragElementIndex, dropLocation: dropPoint};
+    return browser.executeScript(helper.dragAndDrop, dragElementSelector, null, options);
+}
+
+async openElementContextMenu(selector) {
+    return browser.executeScript(helper.mouseRightClick, selector, {elemIndex: 0, location: {x: 100, y: 100}});
+}
+
+mouseUp(pointCoordinates = {x: 100, y: 100}) {
+    return browser.executeScript(helper.mouseUp, pointCoordinates);
+}
