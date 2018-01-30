bd7230d6be1e7b3774a40161086dbe784de331a2
Ihor <ihor.sasovets@gmail.com>
2018-01-30 17:45:21 +0200

    - added mouseDown and mouseMove actions

--- support.js
+++ support.js
@@ -1,11 +1,15 @@
 const dragAndDrop = require('./lib/dragAndDrop'),
 	mouseUp = require('./lib/mouseUp'),
 	mouseClick = require('./lib/mouseClick'),
+	mouseDown = require('./lib/mouseDown'),
+	mouseMove = require('./lib/mouseMove'),
 	rightMouseBtnClick = require('./lib/rightMouseButtonClick');
 
 module.exports = {
 	dragAndDrop: dragAndDrop,
 	mouseUp: mouseUp,
 	mouseClick: mouseClick,
+	mouseDown: mouseDown,
+	mouseMove: mouseMove,
 	rightMouseBtnClick: rightMouseBtnClick
 }
