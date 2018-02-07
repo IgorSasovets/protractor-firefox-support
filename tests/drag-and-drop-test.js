9273f4b424aa9d4e3e498a2fd3ee6d4c333b9c11
Ihor <ihor.sasovets@gmail.com>
2018-02-07 16:38:46 +0200

    - changed mouseClick action, started work on negative tests for DnD

--- tests/drag-and-drop-test.js
+++ tests/drag-and-drop-test.js
@@ -2,23 +2,53 @@
 const support = require('../support');
 const params = browser.params;
 
-describe('Confirm ability to perform drag and drop action in firefox', () => {
-    it('Should perform drag and drop action', () => {
-        const selector = '.btn.btn-primary';
-        const div = $('.btn.btn-primary');
-        const successDiv = $('.btn.btn-success');
-        return browser.get(params.dndTemplateUrl)
+describe('Custom DnD function tests', () => {
+  const div = $('.btn.btn-primary');
+  beforeEach(() => {
+      return browser.get(params.dndTemplateUrl);
+  });
+  describe('Confirm ability to perform drag and drop action in firefox', () => {
+    it('Should perform drag and drop action', function() {
+      const selector = '.btn.btn-primary';
+      const successDiv = $('.btn.btn-success');
+      return browser.wait(protractor.ExpectedConditions.visibilityOf(div), 5000)
+        .then(() => {
+          return browser.executeScript(support.dragAndDrop, selector, null,
+            {dropLocation: {x: 100, y: 150}});
+        })
+        .then(() => {
+            return browser.sleep(2000);
+        })
+        .then(() => {
+            expect(successDiv.isDisplayed()).toBe(true);
+        });
+      });
+  });
+  describe('Negative', () => {
+    describe('Call DnD without required parameters', () => {
+      it('Should confirm that function throw error without specified elements selectors', () => {
+        return browser.wait(protractor.ExpectedConditions.visibilityOf(div), 5000)
           .then(() => {
-              return browser.wait(protractor.ExpectedConditions.visibilityOf(div), 5000);
+            return browser.executeScript(support.dragAndDrop);
           })
-          .then(() => {
-              return browser.executeScript(support.dragAndDrop, selector, null, {dropLocation: {x: 100, y: 150}});
-          })
-          .then(() => {
-              return browser.sleep(2000);
-          })
-          .then(() => {
-              expect(successDiv.isDisplayed()).toBe(true);
+          .catch(err => {
+            console.log(err);
+            expect(err.toString().indexOf('Error!Element selector not defined'))
+              .not.toEqual(-1);
           });
+      });
+      it('Should confirm that function throw error with uncorrect dropElemSelector', () => {
+        let dropElemSelector;
+        return browser.wait(protractor.ExpectedConditions.visibilityOf(div), 5000)
+          .then(() => {
+            return browser.executeScript(support.dragAndDrop, 'dragElemSelector', dropElemSelector,
+              {});
+          })
+          .catch(err => {
+            expect(err.toString().indexOf('Error!Cannot get element with specified selector'))
+              .not.toEqual(-1);
+          });
+      });
     });
+  });
 });
